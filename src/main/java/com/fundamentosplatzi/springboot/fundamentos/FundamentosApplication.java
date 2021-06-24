2ad0ffee4e64cbbc96b97de8b06e6ee8b68eea95
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-06-24 13:19:06 -0300

    Springboot fundamentals - JPA Spring Data

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,13 +4,20 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.fundamentos.entity.User;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import com.fundamentosplatzi.springboot.fundamentos.repository.UserRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
@@ -30,18 +37,23 @@ public class FundamentosApplication implements CommandLineRunner {
 	// Iyectamos nuestra clase UserPojo
 	private UserPojo userPojo;
 
+	// Inyectamos el UserRepository como dependencia
+	private UserRepository userRepository;
+
 	// inyeccion de dependencia en el constructor
 	public FundamentosApplication(@Qualifier("componentSecondImplement") ComponentDependency componentDependency,
 								  MyBean myBean,
 								  MyBeanWithDependency myBeanWithDependency,
 								  MyBeanWithProperties myBeanWithProperties,
-								  UserPojo userPojo)
+								  UserPojo userPojo,
+								  UserRepository userRepository)
 	{
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
 		this.userPojo = userPojo;
+		this.userRepository = userRepository;
 	}
 
 	public static void main(String[] args) {
@@ -50,6 +62,35 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		//ejemplosAnteriores();
+		saveUserInDB();
+		getUserByEmail();
+		getOrderedUsersByName();
+	}
+
+	private void saveUserInDB(){
+		User user1 = new User("Aron", "john@gmail.com", LocalDate.of(2021, 06, 24));
+		User user2 = new User("Alberto", "alberto@gmail.com", LocalDate.of(2021, 06, 24));
+		List<User> users = Arrays.asList(user1, user2);
+
+		// para cada uno de los elementos (user) de la lista lo guardamos en la BD
+		users.stream().forEach(userRepository::save);
+	}
+
+	private void getUserByEmail(){
+		logger.info("User info: " + userRepository.findByUserEmail("alberto@gmail.com")
+				// en caso que no se encuentre un User lanzamos una Exception
+				.orElseThrow(()-> new RuntimeException("No existe User con ese email")));
+	}
+
+	private void getOrderedUsersByName(){
+		userRepository.findAndSort("A", Sort.by("name").ascending())
+			// stream -> por cada elemento de la List mostrams el User)
+			.stream()
+				.forEach(user -> logger.info("User ordered: " + user));
+	}
+
+	private void ejemplosAnteriores(){
 		// ejecutamos la implementacion de la dependencia ComponentDependency
 		componentDependency.saludar();
 
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfig.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfig.java
@@ -5,11 +5,16 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithPropertiesImp
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 @EnableConfigurationProperties(UserPojo.class)
 @Configuration
+@PropertySource("classpath:connection.properties")
 public class GeneralConfig {
     @Value("${value.name}")
     private String name;
@@ -20,9 +25,35 @@ public class GeneralConfig {
     @Value("${value.random}")
     private String random;
 
+    // Llamamos a las propiedades de connection.propertes
+    @Value("${jdb.url}")
+    private String url;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
     // Llamamos a un bean (MyBeanWithProperties) para utilizar nuestras props mapeadas
     @Bean
     public MyBeanWithProperties printProperties(){
         return new MyBeanWithPropertiesImplement(name, lastname);
+    }
+
+    // Hacemos que Springboot nos inyecte esta dependencia con la anotacion @Bean
+    @Bean
+    public DataSource dataSource(){
+        // construimos el DS
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+
+        return dataSourceBuilder.build();
     }
 }
